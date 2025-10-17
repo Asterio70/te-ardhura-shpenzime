28e6ca3c1329104544a410915f92ee24483409ad
Asterio70 <yllikoxha@gmail.com>
2025-10-18 00:37:21 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,1 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyClh-loC4zuSB5N3RYCBQwbowbWdE02JDQk",
+  authDomain: "ardhjeshpenzime.firebaseapp.com",
+  projectId: "ardhjeshpenzime",
+  storageBucket: "ardhjeshpenzime.firebasestorage.app",
+  messagingSenderId: "113999083755",
+  appId: "1:113999083755:web:84af75a6a91fb4e801f84e"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
 
